d456fd830ce24901ace1a590394d23950cca1930
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-05 19:41:49 -0400

    addedscope method for nested task form in group_tasks

--- app/models/group_task.rb
+++ app/models/group_task.rb
@@ -1,5 +1,11 @@
 class GroupTask < ApplicationRecord
   has_many :tasks
 
+  def task=(task_attributes)
+    if task_attributes[:name].present?
+        task = Task.create(task_attributes)
+        self.tasks << task
+      end
+  end
 
 end
