ac14cc81bda85882f078c6c4027f36bbc65f50f0
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-06 19:15:46 -0400

    added users list to index page and fixed syntax error on controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
-class TasksController < ApplicationController
+class UsersController < ApplicationController
   before_action :authenticate_user!
 
      def index
-       @users = User.all 
+       @users = User.all
      end
 
 
