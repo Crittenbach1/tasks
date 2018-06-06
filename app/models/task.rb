54d2a7e22eef60631c9c400c44343294ddeec5fd
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-05 21:47:26 -0400

    created a scope method for task that print status to the view

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :group_task
+
+  def self.status(task)
+     if  task.status == true
+        return "Status: Complete"
+     else
+        return "Status: In Progress"
+     end
+  end
+
 end
