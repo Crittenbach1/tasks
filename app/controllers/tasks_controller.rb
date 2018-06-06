7a01f151d1aa172cc710220283e6150c2c85b04f
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-05 20:05:53 -0400

    set up new controller and new view with a form for a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,13 +7,18 @@ class TasksController < ApplicationController
     end
 
     def new
-
+      @group_task = GroupTask.find_by(id: params[:group_task_id])
+      if @group_task
+        @task = @group_task.tasks.build
+      else
+        redirect_to group_tasks_path
+      end
     end
 
     def create
 
     end
-    
+
       private
         def task_params
 
