6376f1631afc6650783f19de5652098465f89d52
Cynthia Rittenbach <crittenbach@gmail.com>
2018-08-14 18:45:04 -0400

    create display status helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+   def display_status(task)
+     if task.status == true
+        return "Status: Complete"
+     else
+        return "Status: In Progress"
+     end
+   end
+
+
 end
